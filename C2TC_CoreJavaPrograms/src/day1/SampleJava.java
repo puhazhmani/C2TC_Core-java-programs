096cfba2a19a13a9173d88e1172948562af7538c
ACT-C1-01 <ACT-C1-01@DESKTOP-I4SCAC5>
2025-06-12 12:38:58 +0530

    multiplication program

--- C2TC_CoreJavaPrograms/src/day1/SampleJava.java
+++ C2TC_CoreJavaPrograms/src/day1/SampleJava.java
@@ -1,9 +1,17 @@
 package day1;
-
+import java.util.*;
 public class SampleJava {
 
 	public static void main(String[] args) {
-		System.out.println("hello");
+		int mukesh,pugalu,c;
+		Scanner inputvanguren =new Scanner(System.in);
+		System.out.println("a value is :");
+		mukesh=inputvanguren.nextInt();
+		System.out.println("b value is:");
+
+		pugalu=inputvanguren.nextInt();
+		c=mukesh*pugalu;
+		System.out.println(c);
 	}
 
 }
