c2019d4e10ef0dcc8c5f35596eb36c3d02f786c0
ACT-C1-01 <ACT-C1-01@DESKTOP-I4SCAC5>
2025-06-13 12:31:45 +0530

    basic java program

---
+++ C2TC_CoreJavaPrograms/src/demo/Demo.java
@@ -0,0 +1,17 @@
+package demo;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		StudentDemo st=new StudentDemo();
+		st.age=20;
+		st.ageuuu();
+		
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ C2TC_CoreJavaPrograms/src/demo/StudentDemo.java
@@ -0,0 +1,11 @@
+package demo;
+
+public class StudentDemo {
+	int age;
+	public void ageuuu() {
+		System.out.println("my age is:"+age);
+	}
+	
+	
+
+}
