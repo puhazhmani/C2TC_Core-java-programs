6605792a2dcc646b392657c76ef4a00eb66b1e25
ACT-C1-01 <ACT-C1-01@DESKTOP-I4SCAC5>
2025-06-13 11:21:48 +0530

    I have executed basic java program on data types

---
+++ C2TC_CoreJavaPrograms/src/day1/chardemo.java
@@ -0,0 +1,25 @@
+package day1;
+
+public class chardemo {
+
+	public static void main(String[] args) {
+				char ch = 'a';
+				System.out.println(ch);
+
+				char ch1 = 65;
+				System.out.println(ch1);
+
+				char var1 = '\u00A7';
+				System.out.println(var1);
+
+				char var2 = '\u20AC';
+				System.out.println(var2);
+
+				int a = (char)'A';
+				System.out.println(a);
+			}
+		
+
+	
+
+}
